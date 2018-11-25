fb06f37f89065cbafac6772bd27beb5343fd1107
MikeyHampton <45147617+MikeyHampton@users.noreply.github.com>
2018-11-25 04:05:18 -0500

    Update and rename src/Raptor14/MoreCMDs/Main.php to src/main/main.php

--- src/main/main.php
+++ src/main/main.php
@@ -1,28 +1,23 @@
 <?php
-
-namespace Raptor14\MoreCMDs;
-
-use pocketmine\Plugin;
+namespace main;
 use pocketmine\command\CommandExecutor;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\Player;
-use pocketmine\server;
+use pocketmine\Server;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\utils\TextFormat as C;
-
 class Main extends PluginBase implements Listener {
-	public $prefix = C::YELLOW."CMDs".C::DARK_GRAY." >".C::WHITE." ";
+	public $prefix = "CMDs";
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getLogger()->info("MoreCMDs Enabled!");
+		$this->getServer()->getLogger()->info("MoreCMDs Enabled");
 	}
 
 	public function OnCommand(CommandSender $sender, Command $cmd, string $label, array $args) {
 		if($cmd->getName() == "hub"){
-			$sender->sendMessage(C::GREEN . "Teleporting to hub...");
+			$sender->sendMessage("Teleporting To Server Hub");
 			$sender->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
 		}
 	}
